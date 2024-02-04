12e5a17f7211537490a9849fb0d4078244663557
BackEunHo <50124989+BackEunHo@users.noreply.github.com>
2024-02-04 21:56:10 +0900

    [level 2] Title: 올바른 괄호, Time: 7.10 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,8 @@
+def solution(s):
+    count = 0
+    for i in s:
+        if count < 0:
+            break
+        if i == '(': count += 1
+        else: count -= 1
+    return True if count == 0 else False
